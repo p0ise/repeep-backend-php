19ea4eb1e155b80f279d57b47050c74a758e1948
p0ise <changelf@163.com>
2022-11-23 13:15:39 +0800

    fix dependency errors

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 //csrf tracer
-require_once('../client/client_info.php');
 require_once('./model.php');
 
 $k = strtolower(trim(isset($_GET['k']) ? $_GET['k'] : ''));
@@ -178,4 +177,49 @@ function ext_content_type(String $ext)
 
     //默认返回类型
     return "video/x-msvideo";
+}
+
+function get_ip()
+{
+    foreach (array(
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'REMOTE_ADDR'
+    ) as $key) {
+        if (array_key_exists($key, $_SERVER)) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+                $ip = trim($ip);
+                //会过滤掉保留地址和私有地址段的IP，例如 127.0.0.1会被过滤
+                //也可以修改成正则验证IP
+                if ((bool) filter_var(
+                    $ip,
+                    FILTER_VALIDATE_IP,
+                    FILTER_FLAG_IPV4 |
+                        FILTER_FLAG_NO_PRIV_RANGE |
+                        FILTER_FLAG_NO_RES_RANGE
+                )) {
+                    return $ip;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+function get_client_info()
+{
+    $useragent = $_SERVER['HTTP_USER_AGENT'];
+
+    $clientip = get_ip();
+
+    $client_info = array(
+        "ua" => $useragent,
+        "ip" => $clientip,
+    );
+
+    return $client_info;
 }
